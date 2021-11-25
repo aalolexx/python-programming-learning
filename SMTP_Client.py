ec822aa0dc3eb3d93e88e2300f181d019b162c0b
Alex Ehrenhöfer <alexander.ehrenhoefer@student.htw-berlin.de>
2021-11-25 18:08:27 +0100

    initial commit

--- SMTP_Client.py
+++ SMTP_Client.py
@@ -1,48 +1,78 @@
-from socket import *
+## This script connects to googles smtp server and sends a simple mail
 
-msg = "\r\n I love computer networks!"
-endmsg = "\r\n.\r\n"
+import socket
+import base64
 
-# Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver = #Fill in start #Fill in end
+TCP_IP = "localhost"
+TCP_PORT = 25
+BUFFER_SIZE = 1024
+SENDER = "xxxxx"
+RECEIVER = "xxxxx"
+PASSWORD = "xxxxx"
 
-# Create socket called clientSocket and establish a TCP connection with mailserver
-#Fill in start
-#Fill in end
-
-recv = clientSocket.recv(1024).decode()
-print(recv)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+recv = s.recv(1024)
+recv = recv.decode()
 if recv[:3] != '220':
-print('220 reply not received from server.')
+    print("220 reply not received from server.")
 
-# Send HELO command and print server response.
-heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand.encode())
-recv1 = clientSocket.recv(1024).decode()
-print(recv1)
+#
+# say helo
+#
+
+heloCommand = 'helo google\r\n'
+s.send(heloCommand.encode())
+recv1 = s.recv(1024)
+recv1 = recv1.decode()
+print("Message after helo command: " + recv1)
 if recv1[:3] != '250':
-print('250 reply not received from server.')
+    print("250 reply not received from server.")
 
-# Send MAIL FROM command and print server response.
-# Fill in start
-# Fill in end
 
-# Send RCPT TO command and print server response.
-# Fill in start
-# Fill in end
+#
+# Authentification
+#
 
-# Send DATA command and print server response.
-# Fill in start
-# Fill in end
+base64_str = ("\x00"+SENDER+"\x00"+PASSWORD).encode()
+base64_str = base64.b64encode(base64_str)
+authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
+s.send(authMsg)
+recv_auth = s.recv(1024)
+recv_auth = recv_auth.decode()
+print("Message after authentification: " + recv_auth)
+if recv_auth[:3] != '235':
+    print('250 reply not received from server.')
 
-# Send message data.
-# Fill in start
-# Fill in end
+#
+# Defining headers and sending the actual mail
+#
 
-# Message ends with a single period.
-# Fill in start
-# Fill in end
+mailFrom = "MAIL FROM:<"+SENDER+">\r\n"
+s.send(mailFrom.encode())
+recv2 = s.recv(1024)
+recv2 = recv2.decode()
+print("After MAIL FROM command: "+recv2)
+rcptTo = "RCPT TO:<"+RECEIVER+">\r\n"
+s.send(rcptTo.encode())
+recv3 = s.recv(1024)
+recv3 = recv3.decode()
+print("After RCPT TO command: "+recv3)
+data = "DATA\r\n"
+s.send(data.encode())
+recv4 = s.recv(1024)
+recv4 = recv4.decode()
+print("After DATA command: "+recv4)
+subject = "Subject: testing my client\r\n\r\n"
+s.send(subject.encode())
+body = "Test Body content\r\n\r\n.\r\n"
+s.send(body.encode())
+recv_msg = s.recv(1024)
+print("Response after sending message body: "+recv_msg.decode())
+quit = "QUIT\r\n"
+s.send(quit.encode())
+recv5 = s.recv(1024)
+print(recv5.decode())
 
-# Send QUIT command and get server response.
-# Fill in start
-# Fill in end
+# close the connection
+s.close()
